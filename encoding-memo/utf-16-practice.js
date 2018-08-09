1973a590391241ea58cbfbf1a0e3bfaee2b6997c
kuyur <kuyur@kuyur.info>
2018-08-09 19:45:54 +0900

    implement.

---
+++ encoding-memo/utf-16-practice.js
@@ -0,0 +1,15 @@
+// '\u{1d306}' -> 119558 太玄经字符 𝌆
+
+var chr = 0x1d306;
+var m = chr - 0x10000;
+var low = m & 0x3FF;
+var high = m >> 10;
+low += 0xDC00;
+high += 0xD800;
+
+var str = String.fromCharCode(high) + String.fromCharCode(low);
+str.length; // 2
+
+var str2 = '\u{1d306}';
+
+str === str2; // true
