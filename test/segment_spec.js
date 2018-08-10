196d79c71695cd30f352efd2a1e2c64a7de2f107
kuyur <kuyur@kuyur.info>
2018-08-10 14:17:21 +0900

    implement nextc4js.

---
+++ test/segment_spec.js
@@ -0,0 +1,66 @@
+var Segments = require('../lib/nextc4js/segment.js').Segments;
+var test = require('tape');
+
+var segments = new Segments([
+  {
+    "begin": 0,
+    "end": 127,
+    "reference": "ascii",
+    "characterset": "ascii"
+  }, {
+    "begin": 128,
+    "end": 128,
+    "reference": "buffer",
+    "offset": 0,
+    "characterset": "Euro Sign"
+  }, {
+    "begin": 129,
+    "end": 33087,
+    "reference": "undefined",
+    "characterset": "undefined"
+  }, {
+    "begin": 33088,
+    "end": 65535,
+    "reference": "buffer",
+    "offset": 1,
+    "characterset": "GBK"
+  }, {
+    "begin": 65536,
+    "end": 2167439663,
+    "reference": "undefined",
+    "characterset": "undefined"
+  }, {
+    "begin": 2167439664,
+    "end": 2218393145,
+    "condition": ["0x81~0x84", "0x30~0x39", "0x81~0xFE", "0x30~0x39"],
+    "reference": "indexing-buffer",
+    "offset": 32449,
+    "characterset": "Unicode (BMP)"
+  }, {
+    "begin": 2218393146,
+    "end": 2419097903,
+    "reference": "undefined",
+    "characterset": "undefined"
+  }, {
+    "begin": 2419097904,
+    "end": 3812228665,
+    "condition": ["0x90~0xE3", "0x30~0x39", "0x81~0xFE", "0x30~0x39"],
+    "reference": "gb18030-unicode-sp-mapping",
+    "characterset": "Unicode (SP)"
+  }, {
+    "begin": 3812228666,
+    "end": 4294967295,
+    "reference": "undefined",
+    "characterset": "undefined"
+  }
+]);
+
+test('Segments unit test', function(t) {
+  t.test('find', function(assert) {
+    var buffer = new Uint8Array([0x82, 0x35, 0xA0, 0x38]);
+    var find = segments.find(buffer);
+    assert.equal(find.getBegin(), 2167439664);
+    assert.equal(find.getEnd(), 2218393145);
+    assert.end();
+  });
+});
