e81758b14665e1c659fc84cb9e08b936de834c8f
kuyur <kuyur@kuyur.info>
2021-11-03 20:46:00 +0900

    fix linting error.

--- test/load-from-url.spec.js
+++ test/load-from-url.spec.js
@@ -3,8 +3,8 @@ var utils = require('../lib/buffer-utils');
 var { UTF8, UTF16LE } = require('../lib/decoder');
 const fs = require('fs');
 const nodeFetch = require('node-fetch');
-if (!globalThis.fetch) {
-  globalThis.fetch = nodeFetch;
+if (!global.fetch) {
+  global.fetch = nodeFetch;
 }
 
 beforeAll(() => {
@@ -77,23 +77,23 @@ describe('Loading Context unit test', function() {
       expect(decoders_list[2]).toBe('UTF-16BE');
       expect(decoders_list[3]).toBe('GBK(CP936)');
       expect(decoders_list[4]).toBe('BIG5(UAO2.50)');
-  
+
       var encoders_list = context.getEncoderNames();
       expect(encoders_list.length).toBe(3);
       expect(encoders_list[0]).toBe('UTF-8');
       expect(encoders_list[1]).toBe('UTF-16LE');
       expect(encoders_list[2]).toBe('UTF-16BE');
-  
+
       var converters_list = context.getConverterNames();
       expect(converters_list.length).toBe(2);
       expect(converters_list[0]).toBe('simp2tra');
       expect(converters_list[1]).toBe('tra2simp');
-  
+
       var channels_list = context.getChannelNames();
       expect(channels_list.length).toBe(2);
       expect(channels_list[0]).toBe('GBK_to_UTF16-LE_Traditionalized');
       expect(channels_list[1]).toBe('BIG5_to_UTF16-LE_Simplified');
-  
+
       var chann = context.getChannel('GBK_to_UTF16-LE_Traditionalized');
       var gbkBuffer = fs.readFileSync('test/txt/gbk/02-gbk.txt');
       expect(chann.match(gbkBuffer)).toBe(true);
@@ -102,19 +102,19 @@ describe('Loading Context unit test', function() {
       expect(utils.toString(UTF16LE.decode(output))).toBe('任何讀過黑塞作品的人，都會为黑塞作品中的人生阅历與感悟，' +
         '以及浪漫氣息所打動，情不自禁回憶起自己的青年時代。青年沒能在青年時代阅讀黑塞，是一個极大的損失，尽管成年之後，重讀時，' +
         '會感受到這種懊悔，這就是一位只要有過阅讀，就一定會喜歡上的作家，一個性情中人，坦率的朋友，人生的導師。');
-  
+
       var gbk = context.getDecoder('GBK(CP936)');
       var utf16buffer = gbk.decode(gbkBuffer);
       expect(utils.toString(utf16buffer)).toBe('任何读过黑塞作品的人，都会为黑塞作品中的人生阅历与感悟，' +
         '以及浪漫气息所打动，情不自禁回忆起自己的青年时代。青年没能在青年时代阅读黑塞，是一个极大的损失，尽管成年之后，重读时，' +
         '会感受到这种懊悔，这就是一位只要有过阅读，就一定会喜欢上的作家，一个性情中人，坦率的朋友，人生的导师。');
-  
+
       var simp2tra = context.getConverter('simp2tra');
       expect(utils.toString(simp2tra.convert(utf16buffer))).toBe('任何讀過黑塞作品的人，都會为黑塞作品中的人生阅历與感悟，' +
         '以及浪漫氣息所打動，情不自禁回憶起自己的青年時代。青年沒能在青年時代阅讀黑塞，是一個极大的損失，尽管成年之後，重讀時，' +
         '會感受到這種懊悔，這就是一位只要有過阅讀，就一定會喜歡上的作家，一個性情中人，坦率的朋友，人生的導師。');
     });
-    
+
   });
 
   it('loadFromUrl() - gb18030', function() {
@@ -128,22 +128,22 @@ describe('Loading Context unit test', function() {
       expect(decoders_list[1]).toBe('UTF-16LE');
       expect(decoders_list[2]).toBe('UTF-16BE');
       expect(decoders_list[3]).toBe('GB18030');
-  
+
       var encoders_list = context.getEncoderNames();
       expect(encoders_list.length).toBe(4);
       expect(encoders_list[0]).toBe('UTF-8');
       expect(encoders_list[1]).toBe('UTF-16LE');
       expect(encoders_list[2]).toBe('UTF-16BE');
       expect(encoders_list[3]).toBe('GB18030');
-  
+
       var converters_list = context.getConverterNames();
       expect(converters_list.length).toBe(0);
-  
+
       var channels_list = context.getChannelNames();
       expect(channels_list.length).toBe(2);
       expect(channels_list[0]).toBe('UTF-16LE_to_GB18030');
       expect(channels_list[1]).toBe('GB18030_to_UTF-16LE');
-  
+
       var chann = context.getChannel('GB18030_to_UTF-16LE');
       var gb18030Buffer = fs.readFileSync('test/txt/gb18030/02-gb18030.txt');
       expect(chann.match(gb18030Buffer)).toBe(true);
@@ -151,12 +151,12 @@ describe('Loading Context unit test', function() {
       expect(output0).not.toBeNull();
       expect(utils.toString(UTF16LE.decode(output0))).toBe('ḿǹ〾⿰⿱⿲⿳⿴⿵⿶⿷⿸⿹⿺⿻⺁⺄㑳㑇⺈⺋㖞㘚㘎⺌⺗' +
         '㥮㤘㧏㧟㩳㧐㭎㱮㳠⺧⺪䁖䅟⺮䌷⺳⺶⺷䎱䎬⺻䏝䓖䙡䙌䜣䜩䝼䞍⻊䥇䥺䥽䦂䦃䦅䦆䦟䦛䦷䦶䲣䲟䲠䲡䱷䲢䴓䴔䴕䴖䴗䴘䴙䶮');
-  
+
       var gb18030Decoder = context.getDecoder('GB18030');
       var utf16buffer = gb18030Decoder.decode(gb18030Buffer);
       expect(utils.toString(utf16buffer)).toBe('ḿǹ〾⿰⿱⿲⿳⿴⿵⿶⿷⿸⿹⿺⿻⺁⺄㑳㑇⺈⺋㖞㘚㘎⺌⺗' +
       '㥮㤘㧏㧟㩳㧐㭎㱮㳠⺧⺪䁖䅟⺮䌷⺳⺶⺷䎱䎬⺻䏝䓖䙡䙌䜣䜩䝼䞍⻊䥇䥺䥽䦂䦃䦅䦆䦟䦛䦷䦶䲣䲟䲠䲡䱷䲢䴓䴔䴕䴖䴗䴘䴙䶮');
-  
+
       var str = '𝌀𝌁𝌂𝌃𝌄𝌅𝌆𝌇𝌈𝌉𝌊𝌋𝌌𝌍𝌎𝌏𝌐𝌑𝌒𝌓𝌔𝌕𝌖𝌗𝌘𝌙𝌚𝌛𝌜𝌝𝌞𝌟𝌠𝌡𝌢𝌣𝌤𝌥𝌦𝌧𝌨𝌩𝌪𝌫𝌬𝌭𝌮𝌯𝌰𝌱𝌲𝌳𝌴𝌵𝌶𝌷𝌸' +
         '𝌹𝌺𝌻𝌼𝌽𝌾𝌿𝍀𝍁𝍂𝍃𝍄𝍅𝍆𝍇𝍈𝍉𝍊𝍋𝍌𝍍𝍎𝍏𝍐𝍑𝍒𝍓𝍔𝍕𝍖';
       var uint32Buffer = utils.toBuffer(str);
